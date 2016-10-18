50946e1acd530fca163c9e459a71b1a75d89b265
Grahame Bowland <grahame@angrygoats.net>
2016-10-18 16:49:56 +0800

    new filename convention

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-ckan-export",
       name="bpackanexport",
-      version="0.1.0",
+      version="0.2.0",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
